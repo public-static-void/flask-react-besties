89d22ac3e33c379c8f6565e3e330380c6712c992
Vadim Titov <vadim.titov.hh@gmail.com>
2024-06-09 18:40:24 +0200

    fix: changed env to deployment mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
 import { useState } from "react";
 
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 
 function App() {
   const [users, setUsers] = useState([]);
