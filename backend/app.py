896d68ccc0aac89c4c6e53d886623302e4a648cf
Vadim Titov <vadim.titov.hh@gmail.com>
2024-06-09 18:03:32 +0200

    feat: serve react app from backend as static

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask_cors import CORS
+import os
 
 import routes
+from flask import Flask, send_from_directory
+from flask_cors import CORS
 from models import db
 
 app = Flask(__name__)
@@ -11,6 +12,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+
+# Serve static files from the frontend folder
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
 
 
 with app.app_context():
